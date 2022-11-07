37846593112bcc346f8326e4001b55cdd26056f6
Christoph <c.wagner.germany@gmail.com>
2022-11-07 20:28:52 +0100

    new firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'ring-of-fire-3f724',
+    appId: '1:676765705283:web:567d1f247ab1082d9de0f6',
+    storageBucket: 'ring-of-fire-3f724.appspot.com',
+    apiKey: 'AIzaSyC0gA3_PSduLynV1fiQjdLPTmqd3akasc4',
+    authDomain: 'ring-of-fire-3f724.firebaseapp.com',
+    messagingSenderId: '676765705283',
+  }
 };
 
 /*
