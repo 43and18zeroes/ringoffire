9f9c70711f3db7019918dc196565fb192e6201aa
Christoph <c.wagner.germany@gmail.com>
2022-11-15 20:36:02 +0100

    prep portrait lock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ringoffire';
+
+  ngOnInit(): void {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+      && screen.availHeight < screen.availWidth) {
+      
+    }
+  }
+
 }
+
+
+
